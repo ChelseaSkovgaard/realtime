a978be0cd10c4b0c86a7b86ce1b9d1248c206152
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2017-02-01 16:51:20 -0700

    oauth working

---
+++ public/auth0-variables.js
@@ -0,0 +1,3 @@
+var AUTH0_CLIENT_ID='dzhGQQWoNClUt8SuZubXKwSkybzihbgO';
+var AUTH0_DOMAIN='chelseaskovgaard.auth0.com';
+var AUTH0_CALLBACK_URL=location.href;
--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const environment = process.env.NODE_ENV || 'development';
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', process.env.PORT || 3000);
-app.locals.title = 'Secret Box';
+app.locals.title = 'Real Time';
+
+app.use(express.static(path.join(__dirname, '/public')))
 
 app.get('/', (request, response) => {
-  response.send('It\'s a secret to everyone.');
-});
+  response.sendfile(__dirname + '/public/index.html')
+})
+
+app.get('/poll', (request, response) => {
+  response.sendfile(__dirname + '/public/poll.html')
+})
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
+
+module.exports = app;
